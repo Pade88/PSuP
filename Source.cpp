034734daf31232a72082624d2d8363483f10b24e
Pade88 <45396707+Pade88@users.noreply.github.com>
2020-01-11 13:24:43 +0200

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,10 +1,13 @@
 #include "pic18f8722.h" 
 #define DELAY for(int i = 0; i < 10000; i++)
 #define XTAL_FREQ 4000000  
+#define BAUD 9600
+#include <stdio.h>
+#include <string.h>
 
-static int moduleCounter = 0;
 /*
 @Author: OPA
+@Revision: 1.0.0.1 (Non functional changes since 1.0.0.0)
 @Date: 2.0.2020
 @Parameters: Integer conf_EUSART setting UART that follow to be configured <<Range set for only 1 or 2 values>>
 @Inernal pin configuration: 
@@ -14,119 +17,112 @@ static int moduleCounter = 0;
 			SYNC - EUSART Mode selector <<1 for Sync>> | <<0 for aSync>>
 			SPEN - Serial Port Enable <<1 for enable>> | <<0 for holding reset over this pin>>
 			TXEN - Transmit Enable bit <<1 for enable>> | <<0 for disbale>>
-@Variables:
-			badModuleNo - if parameter is inconsistent (different than 1 || 2 ) function set active 
-						EUSART1 or EUSART2 in a random order. (Influenced by number of function calls)
-
-			moduleCounter - it's used as trigger or enable flag, since function does not transmit a flag
-							as parameter, moduleCounter increment at every function call and keeps value
-							since it's a static integer. If current state of moduleCounter is even, program
-							consider it as an ENABLE condition and configure <<conf_EUSART>> pins, otherwise 
-							it is considered as a DISABLE statement and RCSTAxbits & TXSTAxbits are disabled.
+ @Description: Function configure bits for  EUSART COM port 1 or 2  if given parameter is consistent (1 || 2 ).
+ *             If parameters are inconsistent, ASA disable EUSART Communication.
 */
 void EUSART_set(int conf_EUSART)
 {
-	bool badModuleNo = conf_EUSART != 1 && conf_EUSART != 2 ? true : false;
-	if (badModuleNo)
-		conf_EUSART = (moduleCounter % 2) + 1;
-
-	moduleCounter = moduleCounter + 1;
-	if (moduleCounter & 1)
-	{
-		switch (conf_EUSART)
-		{
-		case 1:
-			TXSTA1bits.CSRC = 1;
-			SPBRG1 = 129;
-			TXSTA1bits.BRGH = 1;
-			BAUDCON1bits.BRG16 = 0;
-			TXSTA1bits.SYNC = 0;
-			RCSTA1bits.SPEN = 1;
-			TXSTA1bits.TXEN = 1;
-			break;
-		case 2:
-			TXSTA2bits.CSRC = 1;
-			SPBRG2 = 129;
-			TXSTA2bits.BRGH = 1;
-			BAUDCON2bits.BRG16 = 0;
-			TXSTA2bits.SYNC = 0;
-			RCSTA2bits.SPEN = 1;
-			TXSTA2bits.TXEN = 1;
-		}
-	}
-	else
-	{
-		switch (conf_EUSART)
-		{
-		case 1:
-			RCSTA1bits.SPEN = 0;
-			TXSTA1bits.TXEN = 0;
-			break;
-		case 2:
-			RCSTA2bits.SPEN = 0;
-			TXSTA2bits.TXEN = 0;
-		}
-	}
-	
+    if (conf_EUSART == 1)
+    {
+        TXSTA1bits.CSRC = 1;
+        SPBRG1 = (( XTAL_FREQ/16 ) / BAUD) - 1;  
+        TXSTA1bits.BRGH = 1;
+        BAUDCON1bits.BRG16 = 0;
+        TXSTA1bits.SYNC = 0;   
+        RCSTA1bits.SPEN = 1;   
+        TXSTA1bits.TXEN = 1; 
+    }
+    else if(conf_EUSART == 2)
+    {
+        TXSTA2bits.CSRC = 1;
+        SPBRG2 = (( XTAL_FREQ/16 ) / BAUD) - 1;  
+        TXSTA2bits.BRGH = 1;
+        BAUDCON2bits.BRG16 = 0;
+        TXSTA2bits.SYNC = 0;   
+        RCSTA2bits.SPEN = 1;   
+        TXSTA2bits.TXEN = 1;   
+    }
+    else
+    {  
+        TXSTA1bits.CSRC = 0;
+        TXSTA1bits.BRGH = 0;
+        BAUDCON1bits.BRG16 = 0;
+        TXSTA1bits.SYNC = 0;   
+        RCSTA1bits.SPEN = 0;   
+        TXSTA1bits.TXEN = 0; 
+        TXSTA2bits.CSRC = 0;
+        TXSTA2bits.BRGH = 0;
+        BAUDCON2bits.BRG16 = 0;
+        TXSTA2bits.SYNC = 0;   
+        RCSTA2bits.SPEN = 0;   
+        TXSTA2bits.TXEN = 0;
+    }
 }
 
 
+/*
+ @Author: OPA
+ @Revision 1.0.0.0 (New Entry)
+ @Date 11.01.2020
+ @Parameters: char that follows to be transmitted via EUSART.
+ @Description: ASA use and external integer representing EUSART port number that should transmit.
+ *             ASA send character and apply a delay for consistency.
+ */
+void EUSART_send_char(char c)
+{
+    extern int selectedEUSARTPort;
+    if(selectedEUSARTPort == 1)
+    {
+        TXREG1 = c;
+        DELAY;
+    }
+    else if(selectedEUSARTPort == 2)
+    {
+        TXREG2 = c;
+        DELAY;
+    }
+    else if(selectedEUSARTPort == 0)
+    {
+        ;
+    }
+}
+
+/*
+ @Author: OPA
+ @Revision 1.0.0.0 (New Entry)
+ @Date 11.01.2020
+ @Parameters: A pointer to a char (String) that follows to be transmitted via EUSART.
+ @Description: ASA use EUSART_send_char function to transmit every bit of parameter pointer.
+               When ASA send all the characters from the string, 0x20 is transmitted (0x20 = ASCII code for SPACE)
+ */
+void EUSART_send_String(char *c)
+{
+    for(int i = 0; i<strlen(c); i++)
+    {
+        EUSART_send_char(c[i]);
+    }
+    EUSART_send_char(0x20);
+}
+
+extern int selectedEUSARTPort = 1;
+
 void main(void)  
 {  
-	// SET 1
-	// Testing correct selection of <<selectedUSARTPort>> 1 or 2 and transmitting ASCII characters.
-	unsigned int ASCIIcode = 0x0; 
-	int selectedUSARTPort = 1;
-	EUSART_set(selectedUSARTPort); // ENABLE
+    int loopCounter = 0;
 	while(1) 
 	{  
-		if (selectedUSARTPort == 1)
-		{
-			TXREG1 = ASCIIcode;
-			DELAY;
-		}
-		else if(selectedUSARTPort == 2)
-		{
-			TXREG2 = ASCIIcode;
-			DELAY;
-		}
-
-		ASCIIcode = ASCIIcode + 1;
-
-		if (ASCIIcode > 255)
-			break;
-	}  
-	EUSART_set(selectedUSARTPort); // DISABLE
-
-	// SET 2 
-	// Transmit ASCII characters on EUSART com, disable communication when code is beetween 150 and 200.
-	// Testing ENABLE-DISABLE System and latency.
-	ASCIIcode = 0x0;
-	selectedUSARTPort = 2;
-	EUSART_set(selectedUSARTPort); // ENABLE
-	while (1)
-	{
-		if (selectedUSARTPort == 1)
-		{
-			TXREG1 = ASCIIcode;
-			DELAY;
-		}
-		else if (selectedUSARTPort == 2)
-		{
-			TXREG2 = ASCIIcode;
-			DELAY;
-		}
-
-		ASCIIcode = ASCIIcode + 1;
-
-		if (ASCIIcode == 150)
-		{
-			EUSART_set(selectedUSARTPort); // DISABLE
-		}
-		if (ASCIIcode == 200)
-		{
-			EUSART_set(selectedUSARTPort); // ENABLE
-		}
-
-	}
+        EUSART_set(selectedEUSARTPort);
+        EUSART_send_String("PROIECT SUP");
+        
+        loopCounter = loopCounter + 1;
+        if(loopCounter & 1)
+            selectedEUSARTPort = 2;
+        else
+            selectedEUSARTPort = 1;
+        
+        if(loopCounter > 5)
+        {
+            selectedEUSARTPort = 0;
+        }
+    }
 }
